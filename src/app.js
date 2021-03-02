88d7eb603a693a6cc92358b9c220139764eddf7c
luvagu <luiavag@gmail.com>
2021-03-01 23:24:47 -0500

    added render todos list fn

--- src/app.js
+++ src/app.js
@@ -692,6 +692,32 @@ function renderTodos() {
     }
 }
 
+// Render the todos list
+function renderTodosList() {
+    USER_TODOS.forEach(todo => {
+        const html = `
+            <svg class="h-6 w-6 pointer-events-none" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor">
+                <path d="M9 2a1 1 0 000 2h2a1 1 0 100-2H9z" />
+                <path fill-rule="evenodd" d="M4 5a2 2 0 012-2 3 3 0 003 3h2a3 3 0 003-3 2 2 0 012 2v11a2 2 0 01-2 2H6a2 2 0 01-2-2V5zm3 4a1 1 0 000 2h.01a1 1 0 100-2H7zm3 0a1 1 0 000 2h3a1 1 0 100-2h-3zm-3 4a1 1 0 100 2h.01a1 1 0 100-2H7zm3 0a1 1 0 100 2h3a1 1 0 100-2h-3z" clip-rule="evenodd" />
+            </svg>
+            <span class="ml-3 pointer-events-none">${todo.name}</span>
+        `
+        const selectedClass = ['flex', 'items-center', 'mb-2', 'p-2', 'text-base', 'text-indigo-700', 'bg-gray-200', 'rounded']
+        const normalClass = ['flex', 'items-center', 'mb-2', 'p-2', 'text-base', 'text-gray-600', 'hover:text-gray-700', 'hover:bg-gray-200', 'rounded']
+        
+        const todoElement = document.createElement('a')
+        todoElement.href = '#'
+        todoElement.dataset.todoId = todo.id
+        todoElement.innerHTML = html
+        if (todoElement.dataset.todoId === SELECTED_TODO_LIST_ID) {
+            todoElement.classList.add(...selectedClass)
+        } else {
+            todoElement.classList.add(...normalClass)
+        }
+        todosContainer.appendChild(todoElement)
+    })
+}
+
 
 
 

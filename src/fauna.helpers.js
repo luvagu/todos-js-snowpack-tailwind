cf1629c36c58504b5e9413473da1a361a4e4241f
Luis Vallejo <luiavag@gmail.com>
2021-03-10 14:50:46 -0500

    refactores test functions

--- src/fauna.helpers.js
+++ src/fauna.helpers.js
@@ -5,119 +5,144 @@ const {
     Collection,
 	Create,
 	Delete,
-	Documents,
     Function: Fn,
 	Get,
 	Index,
-    Lambda,
     Login,
     Logout,
-	Map: FMap,
     Match,
-	Paginate,
 	Ref,
     Update,
-    Var
 } = faunadb.query
+
+let DATA_STORE = null
 
 const fClient = new faunadb.Client({ secret: 'fnAED7DuMtACBgvGB0Nwki8Djg0yclrf3UkfGmP4' })
 
-export const createUser = async (firstName, lastName, email, password, tosAgreement = false) => {
-    return await fClient.query(
+export const createUser = async (email, firstName, lastName, password, tosAgreement = false) => {
+    const { data } = await fClient.query(
         Create(
             Collection('users'), 
             { 
                 credentials: { password }, 
-                data: { email, firstName, lastName, selectedListId: 0, todoLists: [], tosAgreement } 
+                data: { email, firstName, lastName, passUpdateTs: 0, selectedListId: 0, todoLists: [], tosAgreement } 
             }
         )
     )
+
+    delete data.passUpdateTs
+    delete data.tosAgreement
+    
+    return data
 }
 
 export const logInUser = async (email, password) => {
-    return await fClient.query(
+    const response = await fClient.query(
         Login(
             Match(
                 Index("users_by_email"), email),
                 { password },
             )
     )
+
+    return {
+        userRef: response.instance.id,
+        userToken: response.secret
+    }
 }
 
-const createSessionToken = (userRef, userToken) => {
-    if (userRef && userToken) {
-        localStorage.setItem('testSessionToken', JSON.stringify({ userRef, userToken }))
+const createSessionToken = (credentials) => {
+    if (typeof(credentials) === 'object' && ('userRef' in credentials) && ('userToken' in credentials)) {
+        localStorage.setItem('testSessionToken', JSON.stringify(credentials))
         return true
     }
+
     return false
 }
 
-const getSessionKeys = () => JSON.parse(localStorage.getItem('testSessionToken')) || null
+const getCredentials = () => JSON.parse(localStorage.getItem('testSessionToken')) || null
 
 export const logOutUser = async (secret) => {
     return await new faunadb.Client({ secret }).query(Logout(true))
 }
 
-export const updateUserAccount = async (firstName, lastName, email, { userRef, userToken: secret }) => {
-    return await new faunadb.Client({ secret }).query(
+export const updateUserAccount = async (email, firstName, lastName, { userRef, userToken: secret }) => {
+    const { data } = await new faunadb.Client({ secret }).query(
         Update(
             Ref(Collection("users"), userRef),
-            { data: { firstName, lastName, email } }
+            { data: { email, firstName, lastName } }
         )
     )
+
+    return {
+        email: data.email, 
+        firstName: data.firstName, 
+        lastName: data.lastName
+    }
 }
 
 export const updateUserPassword = async (password, { userRef, userToken: secret }) => {
-    return await new faunadb.Client({ secret }).query(
+    const response = await new faunadb.Client({ secret }).query(
         Update(
             Ref(Collection("users"), userRef),
-            { credentials: { password } }
+            { 
+                credentials: { password },
+                data: { passUpdateTs: Date.now() * 1000 }
+            }
         )
     )
+
+    return ('data' in response)
 }
 
-export const getUserData = async ({ userRef, userToken: secret }) => {
+export const getLatestUserData = async ({ userRef, userToken: secret }) => {
     const { data } = await new faunadb.Client({ secret }).query(
-        // Call(Fn('getUserDoc'), userRef)
-        Get(Ref(Collection('users'), userRef))
+        Call(Fn('getUserDoc'), userRef)
+        // Get(Ref(Collection('users'), userRef))
         // Paginate(Documents(Collection('users')))
     )
+
+    delete data.tosAgreement
+    delete data.passUpdateTs
 
     return data
 }
 
 export const deleteUserAccount = async ({ userRef, userToken: secret }) => {
-    return await new faunadb.Client({ secret }).query(
+    const { data } = await new faunadb.Client({ secret }).query(
         Delete(Ref(Collection('users'), userRef))
     )
+
+    return data
 }
 
 export const updateUserTodos = async (todoLists, { userRef, userToken: secret }) => {
     if (typeof(todoLists) !== 'object' && !(todoLists instanceof Array)) throw Error('Invalid type of data')
 
-    return await new faunadb.Client({ secret }).query(
+    const { data } = await new faunadb.Client({ secret }).query(
         Update(
             Ref(Collection('users'), userRef), 
             { data: { todoLists } }
         )
     )
+
+    return data.todoLists
 }
 
 export const getUserTodos = async ({ userRef, userToken: secret }) => {
     const { data } = await new faunadb.Client({ secret }).query(
-        Get(Match(Index("all_users")))
+        // Get(Ref(Collection('users'), userRef))
+        Call(Fn('getUserDoc'), userRef)
     )
-
-    // const todos = data.map(doc => {
-    //     const { data } = doc
-    //     data.id = doc.ref.id
-    //     return data
-    // })
     
-    return data
+    return data.todoLists
 }
 
 let first, last, email, password, tos
+
+function toggleLoader() {
+    document.querySelector('#showLoader').classList.toggle('hidden')
+}
 
 document.querySelectorAll('input').forEach(input => {
     switch (input.name) {
@@ -157,96 +182,162 @@ document.querySelectorAll('button').forEach(button =>
     button.addEventListener('click', (e) => {
 
         if (e.target.id === 'suli') {
-            createUser(first, last, email, password, tos)
-                .then(res => {
-                    console.log('res createUser >>>', res.data)
+            toggleLoader()
+            createUser(email, first, last, password, tos)
+                .then(userData => {
+                    DATA_STORE = { ...userData }
+                    console.log('DATA_STORE >>>', DATA_STORE)
 
                     logInUser(email, password)
-                        .then(res => {
-                            console.log('res loginUser >>>', res)
-                            console.log('userToken >>>', res.secret)
-                            console.log('user ref >>>', res.instance.id)
+                        .then(credentials => {
+                            console.log('credentials loginUser >>>', credentials)
 
-                            if (createSessionToken(res.instance.id, res.secret)) {
+                            if (createSessionToken(credentials)) {
                                 console.log('Login success')
                             }
+
+                            toggleLoader()
                         })
                         .catch(e => {
                             console.error('error loginUser >>>', e.message)
+                            toggleLoader()
                         })
                 })
                 .catch(e => {
                     console.error('error createUser >>>', e.message)
+                    toggleLoader()
             })
         }
 
         if (e.target.id === 'li') {
+            toggleLoader()
             logInUser(email, password)
-                .then(res => {
-                    console.log('res loginUser >>>', res)
-                    console.log('userToken >>>', res.secret)
-                    console.log('user ref >>>', res.instance.id)
+                .then(credentials => {
+                    console.log('credentials loginUser >>>', credentials)
 
-                    if (createSessionToken(res.instance.id, res.secret)) {
+                    if (createSessionToken(credentials)) {
                         console.log('Login success')
+
+                        getUserData({ ...credentials })
+                            .then(userData => {
+                                console.log('userData getUserData >>>', userData)
+                                DATA_STORE = { ...userData }
+                                console.log('DATA_STORE >>>', DATA_STORE)
+                                toggleLoader()
+                            })
+                            .catch(e => {
+                                console.error(e.message)
+                                toggleLoader()
+                            })
                     }
                 })
                 .catch(e => {
                     console.error('error loginUser >>>', e.message)
+                    toggleLoader()
                 })
         }
 
+        // all fns below only trigger if session active --->
+
         if (e.target.id === 'ua') {
-            updateUserAccount(first, last, email, { ...getSessionKeys() })
-                .then(m => console.log('updateUserAccount >>>', m))
-                .catch(e => console.error(e.message))
+            toggleLoader()
+            updateUserAccount(email, first, last, { ...getCredentials() })
+                .then(newUserData => {
+                    console.log('newUserData updateUserAccount >>>', newUserData)
+                    DATA_STORE = (DATA_STORE !== null) ? {...DATA_STORE, ...newUserData} : null
+                    console.log('DATA_STORE >>>', DATA_STORE)
+                    toggleLoader()
+                })
+                .catch(e => {
+                    console.error(e.message)
+                    toggleLoader()
+                })
         }
 
         if (e.target.id === 'up') {
-            updateUserPassword(password, { ...getSessionKeys() })
-                .then(m => console.log('updateUserPassword >>>', m))
-                .catch(e => console.error(e.message))
+            toggleLoader()
+            updateUserPassword(password, { ...getCredentials() })
+                .then(updated => {
+                    console.log('updateUserPassword >>>', updated)
+                    toggleLoader()
+                })
+                .catch(e => {
+                    console.error(e.message)
+                    toggleLoader()
+                })
         }
 
         if (e.target.id === 'lo') {
-            logOutUser(getSessionKeys().userToken)
-                .then(s => console.log('Logout', s))
-                .catch(e => console.error(e.message))
+            toggleLoader()
+            logOutUser(getCredentials().userToken)
+                .then(s => {
+                    console.log('Logout >>>', s)
+                    DATA_STORE = null
+                    console.log('DATA_STORE reset >>>', DATA_STORE)
+                    toggleLoader()
+                })
+                .catch(e => {
+                    console.error(e.message)
+                    toggleLoader()
+                })
         }
 
         if (e.target.id === 'gu') {
-            getUserData({ ...getSessionKeys() })
-                .then(user => console.log('getUserData >>>', user))
-                .catch(e => console.error(e.message))
-        }
-
-        if (e.target.id === 'ctd') {
-            createTodo('test2 todo', { ...getSessionKeys() })
-                .then(todo => {
-                    const { data } = todo
-                    data.id = todo.ref.id
-                    console.log('creteTodo >>>', data)
+            toggleLoader()
+            getLatestUserData({ ...getCredentials() })
+                .then(latestUserData => {
+                    console.log('getLatestUserData >>>', latestUserData)
+                    DATA_STORE = (DATA_STORE !== null) ? {...DATA_STORE, ...latestUserData} : null
+                    console.log('DATA_STORE latest >>>', DATA_STORE)
+                    toggleLoader()
                 })
-                .catch(e => console.error(e.message))
+                .catch(e => {
+                    console.error(e.message)
+                    toggleLoader()
+                })
         }
 
         if (e.target.id === 'gutds') {
-            getUserTodos({ ...getSessionKeys() })
-                .then(todos => console.log('getUserTodos >>>', todos))
-                .catch(e => console.error(e.message))
+            toggleLoader()
+            getUserTodos({ ...getCredentials() })
+                .then(todos => {
+                    console.log('getUserTodos >>>', todos)
+                    toggleLoader()
+                })
+                .catch(e => {
+                    console.error(e.message)
+                    toggleLoader()
+                })
         }
 
         if (e.target.id === 'utd') {
-            updateUserTodos([{ id: Date.now(), name: 'First Todo List', tasks: [] }], { ...getSessionKeys() })
-                .then(todo => console.log('updateUserTodos >>>', todo))
-                .catch(e => console.error(e.message))
+            toggleLoader()
+            const newTodo = { id: Date.now(), name: 'First Todo List', tasks: [] }
+            const todoLists = (DATA_STORE !== null && 'todoLists' in DATA_STORE) ? DATA_STORE.todoLists : []
+            todoLists.push(newTodo)
+            DATA_STORE = { ...DATA_STORE, todoLists }
+            updateUserTodos(todoLists, { ...getCredentials() })
+                .then(todo => {
+                    console.log('updateUserTodos >>>', todo)
+                    toggleLoader()
+                })
+                .catch(e => {
+                    console.error(e.message)
+                    toggleLoader()
+                })
         }
 
         if (e.target.id === 'dua') {
-            deleteUserAccount({ ...getSessionKeys() })
-                .then(m => console.log('deleteUserAccount >>>', m))
-                .catch(e => console.error(e.message))
+            toggleLoader()
+            deleteUserAccount({ ...getCredentials() })
+                .then(m => {
+                    console.log('deleteUserAccount >>>', m)
+                    toggleLoader()
+                })
+                .catch(e => {
+                    console.error(e.message)
+                    toggleLoader()
+                })
         }
-        if (e.target.id === '') {}
     })
 )

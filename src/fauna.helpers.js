4302fa51bd8997289c1de5e5f993820a8b3826c9
Luis Vallejo <luiavag@gmail.com>
2021-04-03 11:10:48 -0500

    removed unneeded log

--- src/fauna.helpers.js
+++ src/fauna.helpers.js
@@ -15,7 +15,7 @@ const {
     Update,
 } = faunadb.query
 
-const fClient = new faunadb.Client({ secret: 'fnAEEEVInUACAnRkzzCAo-kg10IUtJUPYCXJs-Jw' })
+const fClient = new faunadb.Client({ secret: 'YOUR_API_KEY' })
 
 export const fSignup = async (email, firstName, lastName, password, tosAgreement = false) => {
     const { data } = await fClient.query(
